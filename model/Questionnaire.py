41cf55cfd06218cf53e50ba32086157d1722d060
WindStation <2399453457@qq.com>
2023-10-11 20:04:58 +0800

    Update: 细节修正

--- model/Questionnaire.py
+++ model/Questionnaire.py
@@ -1,4 +1,5 @@
 class Questionnaire:
-    def __init__(self, questions, answer_recs):
+    def __init__(self, questions, answer_recs, filter_rec):
         self.questions = questions
         self.answer_recs = answer_recs
+        self.filter_rec = filter_rec
